6fe10e2e1fdf1bfdab730617304970e06414ff94
emre2meter <81253909+emre2meter@users.noreply.github.com>
2021-04-03 20:02:18 +0300

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -31,6 +31,16 @@ if (Config.WORKTYPE == 'private') {
 
     }));
 
+    Asena.addCommand({ pattern: 'lovettp ?(.*)', fromMe: true, desc: Lang.GLOW_DESC }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+        var ttinullimage = await axios.get(`https://videfikri.com/api/textmaker/lovemsg/?text1=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
+
+    }));
+
     Asena.addCommand({ pattern: 'pubgttp ?(.*)', fromMe: true, desc: Lang.GLOW_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
