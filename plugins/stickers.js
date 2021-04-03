ee187b09053bf40515a72bd2a7e684b78ba5ee91
emre2meter <81253909+emre2meter@users.noreply.github.com>
2021-04-03 14:41:44 +0300

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -29,6 +29,7 @@ if (Config.WORKTYPE == 'private') {
                 .on('end', async () => {
                     await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
             });
+
         }
 
         ffmpeg(location)
@@ -38,6 +39,7 @@ if (Config.WORKTYPE == 'private') {
             .on('end', async () => {
                 await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
             });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 }
 else if (Config.WORKTYPE == 'public') {
