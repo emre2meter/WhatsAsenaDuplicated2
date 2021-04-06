e219646e7f119e909cd143517903384ed5aec312
emre2meter <81253909+emre2meter@users.noreply.github.com>
2021-04-06 13:58:21 +0300

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -23,6 +23,7 @@ Asena.addCommand({pattern: 'ban ?(.*)', fromMe: true, onlyGroup: true, desc: Lan
     if (Config.BANMSG == 'default') {
         if (message.reply_message !== false) {                                                                                                                                                            
             await message.client.sendMessage(message.jid, fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/giphy_3.mp4"),MessageType.video, { mimetype: Mimetype.gif, caption: 'Founder Tarafından Banlandın!'})
+            await message.client.sendMessage(message.jid,Lang.BANNED + etiketler, MessageType.text, {contextInfo: {mentionedJid: message.mention}});
             await message.client.groupRemove(message.jid, [message.reply_message.data.participant]);
         } else if (message.reply_message === false && message.mention !== false) {
             var etiketler = '';
