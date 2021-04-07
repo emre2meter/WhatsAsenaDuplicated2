1fc012a6a6b24f8d5e25bd7ff24ab4e0754f9a16
emre2meter <81253909+emre2meter@users.noreply.github.com>
2021-04-07 08:45:25 +0300

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,11 +1,3 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
 const simpleGit = require('simple-git');
 const git = simpleGit();
 const Asena = require('../events');
@@ -20,7 +12,7 @@ const Language = require('../language');
 const Lang = Language.getString('updater');
 
 
-Asena.addCommand({pattern: 'update$', fromMe: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'update$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
