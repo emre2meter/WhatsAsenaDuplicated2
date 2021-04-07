d6470ad81610b7bb0dc4aea01f8bf179afa02656
emre2meter <81253909+emre2meter@users.noreply.github.com>
2021-04-07 08:07:37 +0300

    Update location.js

--- plugins/location.js
+++ plugins/location.js
@@ -1,8 +1,3 @@
-/* Codded by @phaticusthiccy
-Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/kyrie.baran
-*/
-
 const Asena = require('../events');
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys'); // Boredom 😬
 
@@ -10,7 +5,7 @@ const Language = require('../language');
 const Lang = Language.getString('locate'); // Language supp. 😉
 
 
-    Asena.addCommand({pattern: 'locate', fromMe: true, desc: Lang.L_DESC, warn: Lang.L_WARN}, (async (message, match) => {
+    Asena.addCommand({pattern: 'locate', fromMe: true, dontAddCommandList: true, desc: Lang.L_DESC, warn: Lang.L_WARN}, (async (message, match) => {
 
         var r_text = new Array ();
         r_text[0] = "degreesLatitude: 24.121231, degreesLongitude: 55.1121221"; // Actually, I don't know where is this place..
