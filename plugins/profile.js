2ea19b4ec99b759080def47aa090b7c996b50660
emre2meter <81253909+emre2meter@users.noreply.github.com>
2021-04-13 23:21:14 +0300

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -2,6 +2,9 @@ const Asena = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
+            await message.client.sendMessage(message.jid, fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/giphy_3.mp4"),MessageType.video, { mimetype: Mimetype.gif, contextInfo: {mentionedJid: [message.reply_message.data.participant]}, caption:   })
+
+
 const fs = require('fs');
 const Language = require('../language');
 const Lang = Language.getString('profile');
@@ -20,7 +23,6 @@ Asena.addCommand({pattern: 'kickme', fromMe: true, dontAddCommandList: true, des
 Asena.addCommand({pattern: 'pp', fromMe: true, dontAddCommandList: true, desc: Lang.PP_DESC}, (async (message, match) => {    
     if (message.reply_message === false || message.reply_message.image === false) return await message.client.sendMessage(message.jid,Lang.NEED_PHOTO);
     
-    var load = await message.client.sendMessage(message.jid,Lang.PPING,MessageType.text);
     var location = await message.client.downloadAndSaveMediaMessage({
         key: {
             remoteJid: message.reply_message.jid,
@@ -30,24 +32,23 @@ Asena.addCommand({pattern: 'pp', fromMe: true, dontAddCommandList: true, desc: L
     });
 
     await message.client.updateProfilePicture(message.client.user.jid, fs.readFileSync(location));
-    await load.delete();
 }));
 
 Asena.addCommand({pattern: 'block ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.BLOCK_DESC}, (async (message, match) => {    
     if (message.reply_message !== false) {
-        await message.client.sendMessage(message.jid, '@' + message.reply_message.jid.split('@')[0] + '```, ' + Lang.BLOCKED + '!```', MessageType.text, {
-            quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+        await message.client.sendMessage(message.jid, fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/giphy_3.mp4"),MessageType.video, {
+            mimetype: Mimetype.mp4, caption: '@' + message.reply_message.jid.split('@')[0] + '```, ' + Lang.BLOCKED + '!```', quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
         });
         await message.client.blockUser(message.reply_message.jid, "add");
     } else if (message.mention !== false) {
         message.mention.map(async user => {
-            await message.client.sendMessage(message.jid, '@' + user.split('@')[0] + '```, ' + Lang.BLOCKED + '!```', MessageType.text, {
-                previewType: 0, contextInfo: {mentionedJid: [user.replace('c.us', 's.whatsapp.net')]}
+            await message.client.sendMessage(message.jid, fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/giphy_3.mp4"),MessageType.video, {
+                mimetype: Mimetype.mp4, caption: '@' + message.reply_message.jid.split('@')[0] + '```, ' + Lang.BLOCKED + '!```', previewType: 0, contextInfo: {mentionedJid: [user.replace('c.us', 's.whatsapp.net')]}
             });
             await message.client.blockUser(user, "add");
         });
     } else if (!message.jid.includes('-')) {
-        await message.client.sendMessage(message.jid, '*' + Lang.BLOCKED_UPPER + '*', MessageType.text);
+        await message.client.sendMessage(message.jid, fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/giphy_3.mp4"),MessageType.video,{caption: '*ENGELLENDİN!*'});
         await message.client.blockUser(message.jid, "add");
     } else {
         await message.client.sendMessage(message.jid, '*' + Lang.NEED_USER + '*', MessageType.text);
